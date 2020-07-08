3db6ab322fd9f76cace5367fab18936855bdf53f
Serlopal <oro_sergio@hotmail.com>
2020-07-08 12:54:59 +0200

    fixed setup to include cache and source using manifest.in

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyvacas",
-    version="0.0.1",
+    name="pyvacas-serlopal",
+    version="0.1",
     author="Serlopal",
     author_email="oro_sergio@hotmail.com",
     description="scrapper that provides easy access to all Spanish holidays",
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Serlopal/pyvacas",
     packages=setuptools.find_packages(),
+    package_data={"cache": ["endpoint.pkl", "holidays.pkl", "locations.pkl"]},
+    py_modules=["pyvacas"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
